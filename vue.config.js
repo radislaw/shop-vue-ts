e00d6f25edaa65ce0682a978907dd828ca875f0e
Yar <radislaw22@gmail.com>
2021-04-17 12:29:38 +1000

    chore: add vue-cli-plugin-style-resources-loader

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+import path from 'path';
+
+module.exports = {
+  pluginOptions: {
+    'style-resources-loader': {
+      preProcessor: 'scss',
+      patterns: [
+        path.resolve(__dirname, './src/assets/styles/*.scss'),
+      ],
+    },
+  },
+};
